28877d0882126b4adc67e0db6c3edbf313f04aaf
evgen <89818121788@mail.ru>
2023-05-30 23:40:34 +0300

    Add Product and ProductsImpl

--- src/main/java/ru/nonton/Main.java
+++ src/main/java/ru/nonton/Main.java
@@ -1,7 +1,25 @@
 package ru.nonton;
 
+import ru.nonton.impl.ProductsImpl;
+import ru.nonton.model.Product;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ProductsImpl products = new ProductsImpl();
+
+        System.out.println(products.addNewProduct(new Product("10", "Test")));
+        System.out.println(products.addNewProduct(new Product("102", "Test")));
+        System.out.println(products.addNewProduct(new Product("101", "Test")));
+        System.out.println(products.addNewProduct(new Product("10", "Test2")));
+        System.out.println(products.addNewProduct(new Product("108", "Test2")));
+        System.out.println(products.addNewProduct(new Product("109", "Test2")));
+        System.out.println(products.deleteProduct(new Product("105", "Test2")));
+        System.out.println(products.deleteProduct(new Product("102", "Test2")));
+        System.out.println(products.findByName("Test"));
+        System.out.println(products.findByName("Test2"));
+        System.out.println(products.findByName("Test3"));
+        System.out.println(products.getProduct("109"));
+        System.out.println(products.getProduct("1019"));
+        System.out.println(products.getProduct("109"));
     }
 }
